160287095e4b1104b5aa51718c8718d9614a6616
Cullen Sharp <cullen.sharp100@gmail.com>
2021-03-12 14:20:33 -0800

    Declare function multiply

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    return [a*b, `The product of ${a} and ${b} is ${a*b}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
