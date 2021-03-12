5ecf70735a5f89645ef2e5664b2c0eb6dba4bac2
Daniel Haugen <me@dnhaugen.com>
2021-03-12 15:29:39 -0800

    solved problem 4

--- app.js
+++ app.js
@@ -79,24 +79,35 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of 
+numbers as its single argument and then returns an array where 
+the first element is the sum of the numbers in the array, and the 
+second element is a string that EXACTLY follows this example and uses the 
+values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. 
+To do addition, use your sum() function that you've already created. You're going to have to be 
+resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, 
+uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let sumArray = sum(sum(testArray[0],testArray[1])[0],testArray[2])[0];
+  return [
+    sumArray,
+    `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${sumArray} is their sum.`
+  ];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -126,7 +137,10 @@ function multiplyArray(multArr) { //eslint-disable-line
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length 
+as its argument and returns an array whose first element is the product of those numbers, 
+and the second element is a string that EXACTLY follows this example and concatenates a 
+message using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
