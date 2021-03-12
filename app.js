649f8416d669cd4f0c3f7a80357282a4284474da
Daniel Haugen <me@dnhaugen.com>
2021-03-12 14:42:41 -0800

    solved problem 2

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ testSum(4, 7);
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments and returns an array where 
+the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
@@ -32,7 +33,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let answer = a * b;
+  return [answer, `The product of ${a} and ${b} is ${answer}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
