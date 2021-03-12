5d06a2ae5cd595388137b5e1c65e4063cc3e97d7
Daniel Haugen <me@dnhaugen.com>
2021-03-12 15:18:07 -0800

    solved problem3

--- app.js
+++ app.js
@@ -44,22 +44,36 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as 
+separate arguments and returns an array where the first element is the sum of 
+those three numbers, the second element is the product of those three numbers,
+and the third and fourth elements are strings that EXACTLY follow this example and use 
+the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. 
+To do addition, use your sum() function, and to do multiplication, use your multiply() function 
+that you've already created. You're going to have to be resourceful to figure out how to do this. 
+However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum3 = sum(sum(a,b)[0],c);
+  let prod3 = multiply(multiply(a,b)[0],c);
+  return [
+    sum3[0],
+    prod3[0],
+    `${a} and ${b} and ${c} sum to ${sum3[0]}.`,
+    `The product of ${a} and ${b} and ${c} is ${prod3[0]}.`
+  ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
