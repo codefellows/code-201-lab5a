32b06d70dc7d745acecf96217ad04e53189876cd
SkylerJohnson102020 <basseuph@gmail.com>
2021-03-12 18:59:43 -0800

    Problem4

--- app.js
+++ app.js
@@ -86,15 +86,23 @@ testSumAndMultiply(4,7,5);
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // // Write your code here
-// let testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; //eslint-disable-line
 
-// function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) { //eslint-disable-line
+     let arrayTotal = 0;
+     let stringTotal = '';
+         for (let i = 0; i < sumArr.length; i++) {
+         arrayTotal = sum(arrayTotal, sumArr[i])[0];
+    }
+    stringTotal = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + arrayTotal + ' is their sum.';
+    
+    return [arrayTotal, stringTotal];
+}
 
-// }
+sumArray(testArray);
+// Here is the test for sumArray(); uncomment it to run it
 
-// // Here is the test for sumArray(); uncomment it to run it
-
-// // testSumArray(testArray);
+testSumArray(testArray);
 
 // // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
