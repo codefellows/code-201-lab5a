9bebefbf8158e76ad2389517539ccbf7c52ec8c6
Sammy Antigua <samiami83@hotmail.com>
2021-03-13 17:45:32 -0500

    pushed to correct branch

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   return [a + b, 'The sum of 4 and 7 is 11.'];
-};
+}
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
