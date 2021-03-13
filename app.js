ebe51b9c880301356a8717e4aa80dfba271c59c8
Daniel Haugen <me@dnhaugen.com>
2021-03-12 16:01:49 -0800

    problem 5 solved

--- app.js
+++ app.js
@@ -113,21 +113,31 @@ testSumArray(testArray);
 
 /////////////////////////////////////
 /* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiplyArray() that takes an array of numbers as its argument 
+and returns an array whose first element is the product of those numbers, and the second 
+element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use 
+your multiply() function that you've already created. You're going to have to be resourceful to figure 
+out how to do this. This function should handle an array containing three elements. However, you may 
+continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, 
+uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  let prodArray = multiply(multiply(multArr[0],multArr[1])[0],multArr[2])[0];
+  return [
+    prodArray,
+    `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${prodArray}.`
+  ];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
