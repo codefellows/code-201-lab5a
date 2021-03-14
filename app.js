9e3bea35f0c25446d052a357b4aa094ef012e1cb
Garinmin <garinmin@gmail.com>
2021-03-13 18:14:07 -0800

    added MultiplyAnyArray

--- app.js
+++ app.js
@@ -126,10 +126,19 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  return [multiplayTask6() , 'The numbers 1,2,3,4,5 have a product of 120.'];
+}
 
+function multiplayTask6(){
+  var i;
+  let result = 1;
+  for (i=0; i<testDynamicArray.length; i++){
+    result = result * testDynamicArray[i];
+  }
+  return result;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
