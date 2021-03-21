ff92c3782ab10d11d1d859d4578408bc04249a9e
ryanhoffman4 <ryanhoffman4@gmail.com>
2021-03-21 03:12:26 -0700

    passed add and multiply test

--- app.js
+++ app.js
@@ -50,11 +50,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum = a + b + c;
+  let product = a * b * c;
+  return [sum, product,  a + ' and ' + b + ' and ' + c + ' sum to ' + sum + '.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + product + '.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
