b87795a28bc3d3001fb473be0bf718fd0067adf0
JALA COX <JALACOX17@GMAIL.COM>
2023-02-27 14:43:12 -0600

    j

--- js/app.js
+++ js/app.js
@@ -88,6 +88,8 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
+
+
 function sumArray(sumArr) { //eslint-disable-line
 
 }
