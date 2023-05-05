32cb331ed03446eccee15a791c9a2f2d4466aa02
Jeffery Flores <jefferyf20@gmail.com>
2023-05-05 16:29:30 -0700

    p2 done

--- js/app.js
+++ js/app.js
@@ -30,11 +30,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let mysum = a * b;
+  let mystr =`The product of ${a} and ${b} is ${mysum}.`;
 
+
+  return [mysum, mystr];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
